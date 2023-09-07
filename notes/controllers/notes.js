7e9a1e65a3d20a2be61bb1ba1beb59d0dca1ba6a
Michal Sostak <michal.sostak@gmail.com>
2023-09-07 21:36:59 +0200

    feat: 13b notes remainder

--- notes/controllers/notes.js
+++ notes/controllers/notes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const jwt = require('jsonwebtoken')
+const { Op } = require('sequelize')
 
 const { Note, User } = require('../models')
 const { SECRET } = require('../util/config')
@@ -19,13 +20,27 @@ const tokenExtractor = (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const notes = await Note.findAll({
+  const where = {}
+
+  if (req.query.important) {
+    where.important = req.query.important === "true"
+  } 
+
+  if (req.query.search) {
+    where.content = {
+      [Op.substring]: req.query.search
+    }
+  }
+
+  const notes = await Note.findAll({ 
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
+
   res.json(notes)
 })
 

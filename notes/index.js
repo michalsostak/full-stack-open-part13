1fb3efa94eca00af2a0964683b1d4102849b5c5d
Michal Sostak <michal.sostak@gmail.com>
2023-09-07 12:36:05 +0200

    feat: part 13a remainder

--- notes/index.js
+++ notes/index.js
@@ -29,10 +29,33 @@ Note.init({
     timestamps: false,
     modelName: 'note'
 })
+Note.sync()
 
 app.get('/api/notes', async (req, res) => {
     const notes = await Note.findAll()
+    console.log(JSON.stringify(notes, null, 2))
     res.json(notes)
+})
+
+app.get('/api/notes/:id', async (req, res) => {
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+        console.log(note.toJSON())
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.put('/api/notes/:id', async (req, res) => {
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+        note.important = req.body.important
+        await note.save()
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
 })
 
 app.post('/api/notes', async (req, res) => {

2ee545fd8aeae4503c3a442eadb4f396d2d91e4b
Michal Sostak <michal.sostak@gmail.com>
2023-09-08 14:05:04 +0200

    feat: exercises 13.10-13.12

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -1,14 +1,38 @@
 const router = require('express').Router()
-const { Blog } = require('../models')
+const jwt = require('jsonwebtoken')
+const { Op } = require('sequelize')
 
+const { Blog, User } = require('../models')
+const { SECRET } = require('../util/config')
 
-router.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body)
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+router.post('/', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const blog = await Blog.create({ ...req.body, userId: user.id })
   return res.status(201).json(blog)
 })
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name', 'username']
+    }
+  })
   console.log(JSON.stringify(blogs, null, 2))
   res.json(blogs)
 })
@@ -29,9 +53,16 @@ router.put('/:id', blogFinder, async (req, res) => {
 })
 
 
-router.delete('/:id', blogFinder, async (req, res) => {
-  await req.blog.destroy()
-  res.status(204).end()
+router.delete('/:id', blogFinder, tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  if (req.blog.userId === user.id) {
+    await req.blog.destroy();
+    res.status(204).end();
+  }
+  else {
+    res.status(401).json({ message: "Unauthorized delete operation" }).end();
+  }
 })
+
 
 module.exports = router
